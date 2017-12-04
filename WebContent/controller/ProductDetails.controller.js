d633fcb107d4ad4ae110be55d70b23c515282d6b
devendervb12 <info@s-max.in>
2017-12-04 21:04:00 +0530

    Create ,update, delete operations in ui5

--- WebContent/controller/ProductDetails.controller.js
+++ WebContent/controller/ProductDetails.controller.js
@@ -17,31 +17,122 @@ sap.ui.controller("smax.batch25.split.controller.ProductDetails", {
 		}, this);
 
 	},
+	
+	onCreate : function(){
+		
+		var oDialog = new sap.m.Dialog({
+			title : "Create a Product",
+			content : [
+				new sap.m.Label({ text : "Product ID"}),
+				new sap.m.Input(),
+				new sap.m.Label({ text : "Name"}),
+				new sap.m.Input(),
+				new sap.m.Label({ text : "Description"}),
+				new sap.m.Input(),
+				new sap.m.Label({ text : "Category"}),
+				new sap.m.Input(),
+				new sap.m.Label({ text : "Supplier ID"}),
+				new sap.m.Input(),
+				
+			],
+			beginButton : new sap.m.Button({ text : "Close", press : function(){
+				oDialog.close();
+			}}),
+			endButton : new sap.m.Button({ text : "Save", press : function(){
+				// send data to SAP
+				var data = {
+						ProductID : oDialog.getContent()[1].getValue(),
+						Name : oDialog.getContent()[3].getValue(),
+						Description : oDialog.getContent()[5].getValue(),
+						Category : oDialog.getContent()[7].getValue(),
+						SupplierID : oDialog.getContent()[9].getValue()
+				}
+				debugger;
+				var oModel = this.getOwnerComponent().getModel();
+				oModel.create("/ProductSet", data, {
+					success : function(){						
+						sap.m.MessageToast.show("Data Created")
+					},
+					error : function(){
+						sap.m.MessageToast.show("Not Created");
+					}
+				});
+				
+				oDialog.close();
+			}.bind(this) })
+		});
+		oDialog.open();	
+		
+	},
+	
+	onUpdate : function(){
+		
+		var oDialog = new sap.m.Dialog({
+			title : "Update Product",
+			content : [
+				new sap.m.Label({ text : "Product ID"}),
+				new sap.m.Input({editable : false}),
+				new sap.m.Label({ text : "Name"}),
+				new sap.m.Input()
+			],
+			endButton : new sap.m.Button({
+				text : "Update and Close",
+				press : function(){
+					var data = {
+							Name : oDialog.getContent()[3].getValue(name)
+					}
+			//	var oModel = this.getOwnerComponent().getModel();	
+					this.getOwnerComponent().getModel().update("/ProductSet('"+oDialog.getContent()[1].getValue(prodID)+"')", data, {
+						success : function(){
+							sap.m.MessageToast.show("Data Updated");
+						},
+						error : function(){
+							sap.m.MessageToast.show("Not Updated");
+						}
+					})
+					oDialog.close();
+				}.bind(this)
+			})
+		});
+		
+		//set the data to NIputs
+		var prodID = this.getView().byId("idobjHeader").getTitle();
+		var name = this.getView().byId("idobjHeader").getIntro();			
+		oDialog.open();
+		oDialog.getContent()[1].setValue(prodID);
+		oDialog.getContent()[3].setValue(name);
+	},
+	
+	onRemove : function(){
+		var prodID = this.getView().byId("idobjHeader").getTitle();
+		
+		this.getOwnerComponent().getModel().remove("/ProductSet('"+prodID+"')", {
+			success : function(){
+				sap.m.MessageToast.show("Data Deleted");
+			},
+			error : function(){
+				sap.m.MessageToast.show("Not Deleted");
+			}
+		})
+	}
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf view.ProductDetails
-*/
-//	onBeforeRendering: function() {
-//
-//	},
-
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf view.ProductDetails
-*/
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf view.ProductDetails
-*/
-//	onExit: function() {
-//
-//	}
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WebContent/controller/ProductList.controller.js
+++ WebContent/controller/ProductList.controller.js
@@ -16,40 +16,14 @@ sap.ui.controller("smax.batch25.split.controller.ProductList", {
 		
 	},
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf view.ProductList
-*/
-//	onBeforeRendering: function() {
-//
-//	},
-
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf view.ProductList
-*/
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf view.ProductList
-*/
-//	onExit: function() {
-//
-//	}
 	
 	onItemPress : function(oEvent){
-	debugger;
+//	debugger;
 	// get the Title
 	var title = oEvent.getSource().getTitle();
 	// pass the data- title(ProductID) to detailView
 	this.getOwnerComponent().getRouter().navTo("detailName", {pid : title})
 	}
-
 });
 
 
